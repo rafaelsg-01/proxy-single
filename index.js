a9a13463e5818714edbd8695826e63d9d41416f4
Rafael Silva <rafaelsgoncalvesbh@gmail.com>
2025-11-05 18:28:32 -0300

    update index.js

--- index.js
+++ index.js
@@ -7,66 +7,72 @@ import { promisify } from 'node:util';
 const pipe = promisify(pipeline);
 
 export const handler = awslambda.streamifyResponse(
-  async (event, responseStream, context) => {
-    
-    const queryParams = event.queryStringParameters || {};
-    const url = queryParams.url;
-    const token = queryParams.token;
+    async (event, responseStream, context) => {
+        const Const_tokenEnv = '123' || process.env.TOKEN_PROXY_SELF;
 
-    // Valida se passou token e se é igual a '123'
-    if (!token || token !== '123') {
-      const errorResponse = {
-        statusCode: 403,
-        body: JSON.stringify({ error: "Token inválido ou ausente" }),
-      };
-      responseStream = awslambda.HttpResponseStream.from(responseStream, errorResponse);
-      responseStream.end();
-      return;
+        const Const_tokenQueryRequest = event.queryStringParameters?.token
+        const Const_urlQueryRequest = event.queryStringParameters?.url
+        const Const_methodRequest = event.requestContext.http.method
+        const Const_bodyRequest = event.body
+
+        if (Const_tokenQueryRequest !== Const_tokenEnv) {
+            const errorResponse = {
+                statusCode: 403,
+                headers: {
+                    "Content-Type": 'text/plain'
+                },
+                body: "Token inválido ou ausente",
+            };
+            responseStream = awslambda.HttpResponseStream.from(responseStream, errorResponse);
+            responseStream.end();
+            return;
+        }
+
+        if (!Const_urlQueryRequest) {
+            const errorResponse = {
+                statusCode: 400,
+                headers: {
+                    "Content-Type": 'text/plain'
+                },
+                body: "Parâmetro ?url é obrigatório",
+            };
+            responseStream = awslambda.HttpResponseStream.from(responseStream, errorResponse);
+            responseStream.end();
+            return;
+        }
+
+        const lib = Const_urlQueryRequest.startsWith('https') ? https : http;
+
+        // Prepara os headers para repassar, se existirem
+        const forwardedHeaders = {};
+        if (event.headers?.authorization) {
+            forwardedHeaders['Authorization'] = event.headers.authorization;
+        }
+        if (event.headers?.['content-type']) {
+            forwardedHeaders['Content-Type'] = event.headers['content-type'];
+        }
+
+        await new Promise((resolve, reject) => {
+        const req = lib.request(Const_urlQueryRequest, { method: Const_methodRequest, headers: forwardedHeaders }, (res) => {
+            const metadata = {
+            statusCode: res.statusCode,
+            headers: {
+                "Content-Type": res.headers['content-type'] || 'text/plain'
+            }
+            };
+            responseStream = awslambda.HttpResponseStream.from(responseStream, metadata);
+
+            pipe(res, responseStream).then(resolve).catch(reject);
+        });
+
+        req.on('error', (err) => reject(err));
+
+        if (Const_methodRequest === 'POST' && Const_bodyRequest) {
+            const body = event.isBase64Encoded ? Buffer.from(Const_bodyRequest, 'base64') : Const_bodyRequest;
+            req.write(body);
+        }
+
+        req.end();
+        });
     }
-
-    if (!url) {
-      const errorResponse = {
-        statusCode: 400,
-        body: JSON.stringify({ error: "Parâmetro ?url é obrigatório" }),
-      };
-      responseStream = awslambda.HttpResponseStream.from(responseStream, errorResponse);
-      responseStream.end();
-      return;
-    }
-
-    const method = event.requestContext.http.method || 'GET';
-    const lib = url.startsWith('https') ? https : http;
-
-    // Prepara os headers para repassar, se existirem
-    const forwardedHeaders = {};
-    if (event.headers?.authorization) {
-      forwardedHeaders['Authorization'] = event.headers.authorization;
-    }
-    if (event.headers?.['content-type']) {
-      forwardedHeaders['Content-Type'] = event.headers['content-type'];
-    }
-
-    await new Promise((resolve, reject) => {
-      const req = lib.request(url, { method, headers: forwardedHeaders }, (res) => {
-        const metadata = {
-          statusCode: res.statusCode,
-          headers: {
-            "Content-Type": res.headers['content-type'] || 'text/plain'
-          }
-        };
-        responseStream = awslambda.HttpResponseStream.from(responseStream, metadata);
-
-        pipe(res, responseStream).then(resolve).catch(reject);
-      });
-
-      req.on('error', (err) => reject(err));
-
-      if (method === 'POST' && event.body) {
-        const body = event.isBase64Encoded ? Buffer.from(event.body, 'base64') : event.body;
-        req.write(body);
-      }
-
-      req.end();
-    });
-  }
 );
