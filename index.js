6ae10a0464405616596ba94d228fcec24e15c3aa
Rafael Silva <rafaelsgoncalvesbh@gmail.com>
2025-11-05 18:53:16 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,78 +1,120 @@
-// index.js
-import http from 'node:http';
-import https from 'node:https';
-import { pipeline } from 'node:stream';
-import { promisify } from 'node:util';
 
-const pipe = promisify(pipeline);
+import http from 'node:http'
+import https from 'node:https'
+import { pipeline } from 'node:stream'
+import { promisify } from 'node:util'
+
+
+const Const_pipe = promisify(pipeline)
+
 
 export const handler = awslambda.streamifyResponse(
-    async (event, responseStream, context) => {
-        const Const_tokenEnv = '123' || process.env.TOKEN_PROXY_SELF;
+    async (Parameter_event, Parameter_responseStream, Const_context) => {
+        // Autenticação \/
+        const Const_tokenEnv = '123' || process.env.TOKEN_PROXY_SELF
 
-        const Const_tokenQueryRequest = event.queryStringParameters?.token
-        const Const_urlQueryRequest = event.queryStringParameters?.url
-        const Const_methodRequest = event.requestContext.http.method
-        const Const_bodyRequest = event.body
+        const Const_tokenQueryRequest = Parameter_event.queryStringParameters?.token
+        const Const_urlQueryRequest = Parameter_event.queryStringParameters?.url
+        const Const_methodRequest = Parameter_event.requestContext.http.method
+        const Const_bodyRequest = Parameter_event.body
 
         if (Const_tokenQueryRequest !== Const_tokenEnv) {
-            const metadata = {
+            //console.log('Invalid token:', Const_tokenQueryRequest)
+            const Const_metadata = {
                 statusCode: 403,
                 headers: {
                     "Content-Type": "text/plain"
                 }
-            };
-            responseStream = awslambda.HttpResponseStream.from(responseStream, metadata);
-            responseStream.write("Token inválido ou ausente");
-            responseStream.end();
-            return;
+            }
+            Parameter_responseStream = awslambda.HttpResponseStream.from(Parameter_responseStream, Const_metadata);
+            Parameter_responseStream.write("Invalid token")
+            Parameter_responseStream.end()
+            return
         }
 
         if (!Const_urlQueryRequest) {
-            const metadata = {
+            //console.log('Missing url parameter')
+            const Const_metadata = {
                 statusCode: 400,
                 headers: {
                     "Content-Type": 'text/plain'
                 }
-            };
-            responseStream = awslambda.HttpResponseStream.from(responseStream, metadata);
-            responseStream.write("Parâmetro ?url é obrigatório");
-            responseStream.end();
-            return;
+            }
+            Parameter_responseStream = awslambda.HttpResponseStream.from(Parameter_responseStream, Const_metadata)
+            Parameter_responseStream.write("Missing url parameter")
+            Parameter_responseStream.end()
+            return
         }
 
-        const lib = Const_urlQueryRequest.startsWith('https') ? https : http;
+        if (Const_methodRequest?.toUpperCase() !== 'GET' && Const_methodRequest?.toUpperCase() !== 'POST') {
+            //console.log('Invalid method:', Const_methodRequest)
+            const Const_metadata = {
+                statusCode: 405,
+                headers: {
+                    "Content-Type": 'text/plain'
+                }
+            }
+            Parameter_responseStream = awslambda.HttpResponseStream.from(Parameter_responseStream, Const_metadata)
+            Parameter_responseStream.write("Method Not Allowed")
+            Parameter_responseStream.end()
+            return
+        }
+        // Autenticação /\
+
+        // Realiza request \/
+        const Const_moduleHttpsOrHttp = Const_urlQueryRequest.startsWith('https') ? https : http
 
         // Prepara os headers para repassar, se existirem
-        const forwardedHeaders = {};
-        if (event.headers?.authorization) {
-            forwardedHeaders['Authorization'] = event.headers.authorization;
-        }
-        if (event.headers?.['content-type']) {
-            forwardedHeaders['Content-Type'] = event.headers['content-type'];
+        const Const_headersAuthorizationRequest = Parameter_event.headers?.['Authorization'] || Parameter_event.headers?.['authorization']
+        const Const_headersContentTypeRequest = Parameter_event.headers?.['Content-Type'] || Parameter_event.headers?.['content-type']
+        
+        let Let_urlFetch = ''
+        let Let_requestInitFetch = {}
+
+        if (Const_urlQueryRequest) {
+            Let_urlFetch = Const_urlQueryRequest
         }
 
-        await new Promise((resolve, reject) => {
-        const req = lib.request(Const_urlQueryRequest, { method: Const_methodRequest, headers: forwardedHeaders }, (res) => {
-            const metadata = {
-                statusCode: res.statusCode,
-                headers: {
-                    "Content-Type": res.headers['content-type'] || 'text/plain'
+        if (Const_methodRequest) {
+            Let_requestInitFetch.method = Const_methodRequest
+        }
+
+        if (Const_headersAuthorizationRequest) {
+            Let_requestInitFetch.headers = {
+                'Authorization': Const_headersAuthorizationRequest,
+                ...Let_requestInitFetch.headers
+            }
+        }
+
+        if (Const_headersContentTypeRequest) {
+            Let_requestInitFetch.headers = {
+                'Content-Type': Const_headersContentTypeRequest,
+                ...Let_requestInitFetch.headers
+            }
+        }
+
+        await new Promise((Parameter_resolve, Parameter_reject) => {
+            const Const_responseFetch = Const_moduleHttpsOrHttp.request(Const_urlQueryRequest, Let_requestInitFetch, (Parameter_responseFetch) => {
+                const Const_metadata = {
+                    statusCode: Parameter_responseFetch.statusCode,
+                    headers: {
+                        "Content-Type": Parameter_responseFetch.headers['content-type'] || 'text/plain'
+                    }
                 }
-            };
-            responseStream = awslambda.HttpResponseStream.from(responseStream, metadata);
+                Parameter_responseStream = awslambda.HttpResponseStream.from(Parameter_responseStream, Const_metadata)
 
-            pipe(res, responseStream).then(resolve).catch(reject);
-        });
+                Const_pipe(Parameter_responseFetch, Parameter_responseStream).then(Parameter_resolve).catch(Parameter_reject)
+            })
 
-        req.on('error', (err) => reject(err));
+            Const_responseFetch.on('error', (Parameter_error) => Parameter_reject(Parameter_error))
 
-        if (Const_methodRequest === 'POST' && Const_bodyRequest) {
-            const body = event.isBase64Encoded ? Buffer.from(Const_bodyRequest, 'base64') : Const_bodyRequest;
-            req.write(body);
-        }
+            if (Const_methodRequest === 'POST' && Const_bodyRequest) {
+                const Const_body = Parameter_event.isBase64Encoded ? Buffer.from(Const_bodyRequest, 'base64') : Const_bodyRequest
+                Const_responseFetch.write(Const_body)
+            }
 
-        req.end();
-        });
+            Const_responseFetch.end()
+        })
+        // Realiza request /\
     }
-);
+)
